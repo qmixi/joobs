b73b163d14cbd3dfb1163842ee50dc75c42556e3
Piotr Kumorek <piotr.kumi@gmail.com>
2018-07-17 22:04:52 +0200

    Started integration with swipe functionality

--- src/screens/DeckScreen.js
+++ src/screens/DeckScreen.js
@@ -1,21 +1,65 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Platform} from 'react-native';
 
 import {connect} from 'react-redux';
+import {MapView} from "expo";
+
+import Swipe from "../components/Swipe/Swipe";
+import {Card} from "react-native-elements";
+
 
 class DeckScreen extends Component {
+		renderCard(job) {
+				const initialRegion = {
+					longitude: job.longitude,
+					latitude: job.latitude,
+					longitudeDelta: 0.045,
+					latitudeDelta: 0.09,
+				};
+
+				return <Card title={job.jobtitle}>
+						<View style={{height: 300}}>
+								<MapView
+										scrollEnabled={false}
+										style={{flex: 1}}
+										cacheEnabled={Platform.OS === 'android' ? true : false}
+										initialRegion={initialRegion}
+								/>
+						</View>
+						<View style={styles.detailsWrapper}>
+								<Text>{job.company}</Text>
+								<Text>{job.formattedRelativeTime}</Text>
+						</View>
+						<Text>{job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}</Text>
+				</Card>
+		};
+
+		renderNoMoreCards() {
+				return (
+						<Card title="No more jobs"/>
+				)
+		}
+
 		render() {
 				return (
 						<View>
-								<Text>Deck Screen</Text>
-								<Text>Deck Screen</Text>
-								<Text>Deck Screen</Text>
-								<Text>Deck Screen</Text>
-								<Text>Deck Screen</Text>
+								<Swipe
+										data={this.props.jobs}
+										renderCard={this.renderCard}
+										renderNoMoreCards={this.renderNoMoreCards}
+								/>
 						</View>
 				);
 		}
 }
+
+const styles = {
+		detailsWrapper: {
+				flexDirection: 'row',
+				justifyContent: 'space-around',
+				marginBottom: 10
+		}
+};
 
 function mapStateToProps({jobs}) {
 		return {
